5de49eaa8801c3c92f38bd8925594365a2b1bb7e
James <jameskarlsson1@gmail.com>
2021-04-02 20:41:10 +1100

    Cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,15 @@
 module.exports = {
   purge: {
-    enabled: true,
+    enabled: process.env.NODE_ENV === 'production',
     safeList: [],
     content: ['./index.html', './src/**/*.tsx', './src/**/*.ts'],
   },
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    minWidth: {
-      40: '10rem',
-      60: '15rem',
-      80: '20rem',
-      100: '25rem',
-    },
-    maxWidth: {
-      120: '30rem',
-      160: '40rem',
-      200: '50rem',
-    },
+    extend: {},
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
 };
