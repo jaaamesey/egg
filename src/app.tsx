5466f61e6796f31b27f1ede5afcebe5cf7b65af3
James Karlsson <jameskarlsson1@gmail.com>
2021-04-04 21:51:53 +1000

    FINALLY fixed canvas scaling

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { EggCanvas } from './ui/base/egg_canvas';
 import { TopMenu } from './ui/top_menu';
 import { BottomMenu } from './ui/bottom_menu';
 
@@ -9,14 +10,34 @@ const App = () => {
       <Switch>
         <Route exact path="/">
           <div
-            className="h-full flex flex-col justify-between"
+            className="h-full"
             style={{
+              display: 'grid',
+              maxWidth: '100%',
+              gridTemplateColumns: '1fr',
+              gridTemplateRows: '1fr',
               backgroundImage:
                 'url(https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F07%2Fel-yunque-national-rainforest-tropical-puerto-rico-TROPICALPLANTS0617.jpg)',
             }}
           >
-            <TopMenu />
-            <BottomMenu />
+            <div
+              style={{
+                height: '100%',
+                maxWidth: '100%',
+                gridRowStart: 1,
+                gridColumnStart: 1,
+                overflow: 'hidden',
+              }}
+            >
+              <EggCanvas />
+            </div>
+            <div
+              className="h-full flex flex-col justify-between"
+              style={{ gridRowStart: 1, gridColumnStart: 1 }}
+            >
+              <TopMenu />
+              <BottomMenu />
+            </div>
           </div>
         </Route>
       </Switch>
--- src/ui/base/egg_canvas.tsx
+++ src/ui/base/egg_canvas.tsx
@@ -1,10 +1,38 @@
+import type * as THREE from 'three';
+
 import React from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+export const Egg = () => {
+  // This reference will give us direct access to the mesh
+  const mesh = React.useRef<THREE.Mesh>();
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = React.useState(false);
+  const [active, setActive] = React.useState(false);
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame((state, delta) => mesh?.current && (mesh.current.rotation.x += 0.01));
+  // Return view, these are regular threejs elements expressed in JSX
+  return (
+    <mesh
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+};
 
 export const EggCanvas = () => {
-  const [canvasEl, setCanvasEl] = React.useState<HTMLCanvasElement | null>(null);
-  React.useEffect(() => {
-    console.log(canvasEl);
-    if (!canvasEl) return;
-  }, [canvasEl]);
-  return <canvas ref={(el) => setCanvasEl(el)} />;
+  return (
+    <div style={{ height: '100%' }}>
+      <Canvas>
+        <ambientLight />
+        <Egg />
+      </Canvas>
+    </div>
+  );
 };
