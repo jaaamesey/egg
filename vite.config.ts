7ef42c91a45127efee33791efac1f41487410153
James Karlsson <jameskarlsson1@gmail.com>
2021-04-05 13:09:47 +1000

    Add brotli compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import brotli from 'rollup-plugin-brotli';
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), brotli()],
 });
